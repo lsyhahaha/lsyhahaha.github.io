dddfb9ced3348d94253092955fe22cb9cd08c792
lsyhahaha <987083256@qq.com>
2023-11-24 20:46:24 +0800

    add

---
+++ _codes/test.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void dfs(int step) {
+    if (递归终止条件) {
+        
+    }
+
+    // 尝试每一种可能
+    for (int i = 1; i <= n; i++){
+        // 继续下一步
+        dfs(step + 1)
+    }
+    return;
+}
